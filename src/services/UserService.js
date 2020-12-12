b6709d8522ace4810d2b7ebec8ef368e325ad7a4
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-12-12 02:50:24 +0100

    e

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -6,7 +6,7 @@ class UserService {
 
   constructor() {
     let service = axios.create({
-      baseURL: "http://88.3.13.99:3000/",
+      baseURL: "https://88.3.13.99:3000/",
       withCredentials: true
     });
 
